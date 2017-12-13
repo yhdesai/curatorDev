c7b2d9b92fb62e0dd4502f4cfa6fed7989fb72b5
Yash <yhdelectric@gmail.com>
2017-12-13 22:27:24 +0530

    added swipe listener need to add activity on swipe

--- app/src/main/java/io/github/yhdesai/curator/DeveloperKey.java
+++
@@ -1,9 +0,0 @@
-package io.github.yhdesai.curator;
-
-/**
- * Created by yash on 11/12/17.
- */
-
-public class DeveloperKey {
-    public static final String DEVELOPER_KEY = "AIzaSyD3GTcj6SynuVTH_SThV0rbLhAs4IaJOGs";
-}
